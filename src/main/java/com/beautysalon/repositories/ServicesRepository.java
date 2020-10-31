314f77f0bb32195b749e08a8eab7f5f37c2cad2a
Cacti79 <mehdi1020@yahoo.com>
2020-10-31 12:18:41 +0100

    init

--- src/main/java/com/beautysalon/repositories/ServicesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.beautysalon.repositories;
-import com.beautysalon.models.Services;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ServicesRepository extends JpaRepository<Services, Long> {
-
-}
